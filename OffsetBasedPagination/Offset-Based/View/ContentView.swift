085ee010a3d826d0df0000bf9526a1da68222aca
aditybikramswain <adityabikram1999@gmail.com>
2025-10-27 14:44:11 +0530

    folder add

---
+++ OffsetBasedPagination/Offset-Based/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  PaginationI
+//
+//  Created by Apple on 27/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
