b2362e7bf5f0fe5af36035ba1a6366812b4bec88
aditybikramswain <adityabikram1999@gmail.com>
2025-10-27 14:23:26 +0530

    Add Xcode project

---
+++ PaginationI/PaginationI/PaginationIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationIApp.swift
+//  PaginationI
+//
+//  Created by Apple on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PaginationIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
